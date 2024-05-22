f5805fba0e78c2233dce2862b54247bc3da1d8f2
Anandhanatarajan Kuppusamy <anandanand1986@gmail.com>
2024-05-22 22:17:40 +0530

    Unique Morse Code

---
+++ src/Arrays/Hashing/UniqueMorseCodes.java
@@ -0,0 +1,25 @@
+package Arrays.Hashing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueMorseCodes {
+    String[] morseCodes = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    Set<String> wordSet = new HashSet<>();
+    int    uniqueMorse   = 0;
+    public int uniqueMorseRepresentations(String[] words) {
+        for(String word : words){
+            StringBuilder strBuilder = new StringBuilder();
+            char[]  wordCharArray = word.toCharArray();
+            for(char temp : wordCharArray){
+                strBuilder.append(morseCodes[temp-'a']);
+            }
+            if(!wordSet.contains(strBuilder.toString())){
+                uniqueMorse++;
+                wordSet.add(strBuilder.toString());
+                strBuilder.setLength(0);
+            }
+        }
+        return     uniqueMorse;
+    }
+}
